39be416a326e1ff3734ae0cca78f0a55a97d55f4
tamyhelo123 <nunestamily66@gmail.com>
2024-10-28 12:33:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,40 @@
+criaCartao(
+    'quem so eu?',
+    'me chamo tamily tenho 15 anos faço aniversario dia 22 de dezembro amo pescar no meu tempo livre',
+    'minha familia em '
+)
+criaCartao(
+    'minha familia',
+    'como eles são',
+    'minha familia em comum eles ce dão ate que bem amam contar piadas fazer as pessoas rirem '
+)
+criaCartao(
+    'ondo eu moro',
+    'como é ',
+    'moro em Pinhão no bairro 2 irmãos moro la ah 14 anos como que é e bom um bairro tranquilo vizinhos otimos 25°43'15"S 51°37'38'
+)
+criaCartao(
+    'onde estudo',
+    '2B sala 5',
+    'estudo no morski ah 6 anos e um otimo colegio '
+) 
+criaCartao(
+    'minha turma',
+    'como é sua turma',
+    'minha sala e barulhenta muita risada e fofoca '
+)
+criaCartao(
+    'faculdade',
+    'o que predender cursar',
+    'pretendo fazer uma faculdade de pedagogia e direitos e depois seguir a carreira de PM'
+)
+criaCartao(
+    'cidade',
+    'como é sua cidade',
+    'minha cidade esta se desenvolvendos varios projetos,areas de lazer para passear cidade peuqena mais bom de morar'
+)
+criaCartao(
+    'meu sonho',
+    'qual e o seu sonho',
+    'conquistar os meus objetivos'
+)
